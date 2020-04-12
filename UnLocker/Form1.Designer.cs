816e849e3c05a4cc112ceed4a709eab37d66a8f8
Marek Witkowski <marek_witkowski@hotmail.com>
2020-04-12 22:24:54 +0200

    Final version.

--- UnLocker/Form1.Designer.cs
+++ UnLocker/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UnLocker
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cboYear = new System.Windows.Forms.ComboBox();
+            this.txtDocNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Odblokuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "&Zakończ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cboYear
+            // 
+            this.cboYear.FormattingEnabled = true;
+            this.cboYear.Items.AddRange(new object[] {
+            "2020",
+            "2019",
+            "2018",
+            "2017",
+            "2016",
+            "2015",
+            "2014",
+            "2013",
+            "2012",
+            "2011",
+            "2010"});
+            this.cboYear.Location = new System.Drawing.Point(12, 46);
+            this.cboYear.Name = "cboYear";
+            this.cboYear.Size = new System.Drawing.Size(121, 21);
+            this.cboYear.TabIndex = 2;
+            // 
+            // txtDocNumber
+            // 
+            this.txtDocNumber.Location = new System.Drawing.Point(139, 48);
+            this.txtDocNumber.Name = "txtDocNumber";
+            this.txtDocNumber.Size = new System.Drawing.Size(160, 20);
+            this.txtDocNumber.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rok dokumentu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Numer dokumentu:";
+            // 
+            // labStatus
+            // 
+            this.labStatus.AutoSize = true;
+            this.labStatus.Location = new System.Drawing.Point(12, 138);
+            this.labStatus.Name = "labStatus";
+            this.labStatus.Size = new System.Drawing.Size(46, 13);
+            this.labStatus.TabIndex = 6;
+            this.labStatus.Text = "Gotowy!";
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 173);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(394, 163);
+            this.Controls.Add(this.labStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDocNumber);
+            this.Controls.Add(this.cboYear);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMain";
+            this.Text = "Odblokowywacz";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cboYear;
+        private System.Windows.Forms.TextBox txtDocNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labStatus;
     }
 }
 
--- UnLocker/Form1.cs
+++ UnLocker/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,42 @@ using System.Windows.Forms;
 
 namespace UnLocker
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String year = cboYear.Text;
+            String docNumber = txtDocNumber.Text;
+            string pathToFile = $"L:/progvat/hl/rch/{docNumber}{year.Substring(2,2)}.TMP";
+            try
+            {
+
+                File.Delete(pathToFile);
+                labStatus.Text = "Dokument " + docNumber + " odblokowany.";
+
+            }
+
+            catch
+
+            {
+
+                labStatus.Text = "Nie można odblokować dokumentu!";
+
+            }
+
         }
     }
 }
